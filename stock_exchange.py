dc455de60a5767f6c6cc5ecc59c59834802db0d3
jsilva83 <jorge.h.silva@gmail.com>
2022-01-05 07:37:43 +0000

    - Version 1.0 complete.

--- stock_exchange.py
+++ stock_exchange.py
@@ -6,6 +6,22 @@ import send_sms as snd
 # Constants.
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+STK_VARIANCE = 0.03  # About 3% variance.
+
+
+def create_sms_message(in_stk_variance: float, in_tesla_news_list: list) -> str:
+    out_sms_message = ''
+    if in_stk_variance < 0:
+        out_sms_message += f'TSLA: 🔻{round(abs(in_stk_variance * 100), 1)}%\n'
+    else:
+        out_sms_message += f'TSLA: 🔺{round(abs(in_stk_variance * 100), 1)}%\n'
+    # Headline 1.
+    out_sms_message += f"Headline: {in_tesla_news_list[0]['title']}\n"
+    out_sms_message += f"Brief: {in_tesla_news_list[0]['description']}\n"
+    # Headline 2.
+    out_sms_message += f"Headline: {in_tesla_news_list[1]['title']}\n"
+    out_sms_message += f"Brief: {in_tesla_news_list[1]['description']}\n"
+    return out_sms_message
 
 
 def stock_exchange():
@@ -13,18 +29,21 @@ def stock_exchange():
     # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
     tesla_stock_obj = stk.StockExchange('TSLA')
     tesla_stock_dict = tesla_stock_obj.get_last_2_days()
-    print(tesla_stock_dict)
-
-    # STEP 2: Use https://newsapi.org
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-    tesla_news_obj = news.News('Tesla')
-    tesla_news_list = tesla_news_obj.get_last_headlines(3)
-    print(tesla_news_list)
-
-    # STEP 3: Use https://www.twilio.com
-    # Send a seperate message with the percentage change and each article's title and description to your phone number.
-    sms_sender = snd.TwilioSMS()
-    sms_sender.send_sms('Put here the message to send.')
+    # See data example in json_data_samples.json
+    stk_value_d_1, stk_value_d_2 = (float(item['4. close']) for item in tesla_stock_dict.values())
+    stk_variance = (stk_value_d_2 / stk_value_d_1) - 1
+    if abs(stk_variance) > STK_VARIANCE:
+        # STEP 2: Use https://newsapi.org
+        # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+        tesla_news_obj = news.News('Tesla')
+        tesla_news_list = tesla_news_obj.get_last_headlines(3)
+        # Create message.
+        sms_message_to_send = create_sms_message(stk_variance, tesla_news_list)
+        # STEP 3: Use https://www.twilio.com
+        # Send a seperate message with the percentage change and each article's title and
+        # description to your phone number.
+        sms_sender = snd.TwilioSMS()
+        sms_sender.send_sms(sms_message_to_send)
     return
 
 # Optional: Format the SMS message like this:
